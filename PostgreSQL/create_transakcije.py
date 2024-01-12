f7fc3a21361f65e28fe1a1a6e7aaed2ae6a14c43
T30M47 <teo.matijasic@student.uniri.hr>
2024-01-12 18:30:57 +0100

    Dodano kreiranje lažnih podataka za transakcije.

--- PostgreSQL/create_transakcije.py
+++ PostgreSQL/create_transakcije.py
@@ -56,7 +56,7 @@ def create_fake_proizvodi():
     conn.commit()
 
 def generate_random_price():
-    format_choices = ["{:.0f}", "{:.2f}", "{:.2f}", "{:.2f}"]
+    format_choices = ["{:.0f}", "{:.1f}", "{:.2f}"]
     selected_format = random.choice(format_choices)
     return float(selected_format.format(random.uniform(10, 1000)))
 
@@ -68,9 +68,13 @@ def generate_variation_of_company(company_name):
     ]
     return random.choice(variations)
 
-def generate_product_category():
-    product_categories = ["Electronics", "Clothing", "Home and Garden", "Beauty", "Books", "Toys", "Sports", "Shoes"]
-    return random.choice(product_categories)
+def generate_random_popust():
+    with_percentage = random.choice([True, False])
+
+    if with_percentage:
+        return f"{random.randint(1, 50)}%"
+    else:
+        return random.randint(1, 50)
 
 trgovine_nazivi = ["Konzum", "Super-Maxi Konzum", "Mini-Konzum", "MultiplusZum", "ExtraKonzum"]
 lokacije_nazivi = ["Zagreb", "Rijeka", "Osijek", "Rijeka", "Split"]
@@ -105,21 +109,63 @@ def create_fake_trgovine():
     print("Data inserted in Trgovine!")
     conn.commit()
 
-create_Transakcije_table = """
-CREATE TABLE IF NOT EXISTS Transakcije (
-    id_transakcije INTEGER PRIMARY KEY,
-    barkod_id INTEGER REFERENCES Proizvodi(barkod_id),
-    id_trgovine INTEGER REFERENCES Trgovine(id_trgovine),
-    kolicina INTEGER NOT NULL,
-    ukupna_cijena NUMERIC(10, 2) NOT NULL,
-    popust INTEGER
-);
-"""
+
+def create_fake_transakcije():
+    inserted_transactions = set()
+
+    create_Transakcije_table = """
+    CREATE TABLE IF NOT EXISTS Transakcije (
+        id_transakcije INTEGER PRIMARY KEY,
+        barkod_id INTEGER REFERENCES Proizvodi(barkod_id),
+        id_trgovine INTEGER REFERENCES Trgovine(id_trgovine),
+        kolicina INTEGER NOT NULL,
+        ukupna_cijena DOUBLE PRECISION NOT NULL,
+        popust VARCHAR(5)
+    );
+    """
+    cursor.execute(create_Transakcije_table)
+    conn.commit()
+
+    for _ in range(5000):
+        while True:
+            id_transakcije = random.randint(1000000, 9999999)
+            if id_transakcije not in inserted_transactions:
+                break
+        barkod_id = get_random_barkod_id()
+        id_trgovine = get_random_id_trgovine()
+        kolicina = random.randint(1, 10)
+        ukupna_cijena = generate_random_price()
+        popust = generate_random_popust()
+        # Insert row
+        insert_data_query = """
+            INSERT INTO Transakcije (id_transakcije, barkod_id, id_trgovine, kolicina, ukupna_cijena, popust)
+            VALUES (%s, %s, %s, %s, %s, %s);
+        """
+        inserted_transactions.add(id_transakcije)
+        cursor.execute(insert_data_query, (id_transakcije, barkod_id, id_trgovine, kolicina, ukupna_cijena, popust))
+
+        if len(inserted_transactions) == 500 or len(inserted_transactions) == 600:
+                      cursor.execute(insert_data_query, (random.randint(10000000, 99999999), barkod_id, id_trgovine, kolicina, ukupna_cijena, popust))
+
+    print("Data inserted in Transakcije!")
+    conn.commit()
+
+def get_random_barkod_id():
+    # Function to query and return a valid barkod_id from the Proizvodi table
+    cursor.execute("SELECT barkod_id FROM Proizvodi ORDER BY RANDOM() LIMIT 1;")
+    result = cursor.fetchone()
+    return result[0] if result else None
+
+def get_random_id_trgovine():
+    # Function to query and return a valid id_trgovine from the Trgovine table
+    cursor.execute("SELECT id_trgovine FROM Trgovine ORDER BY RANDOM() LIMIT 1;")
+    result = cursor.fetchone()
+    return result[0] if result else None
 
 # Execute table creation queries
 create_fake_proizvodi()
 create_fake_trgovine()
-cursor.execute(create_Transakcije_table)
+create_fake_transakcije()
 
 # Commit the changes and close the connection
 conn.commit()
