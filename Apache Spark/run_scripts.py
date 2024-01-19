dd33b44f2d0ae9b068e9a14b34121f5142584deb
T30M47 <teo.matijasic@student.uniri.hr>
2024-01-19 16:51:22 +0100

    Dodana skripta za pokretanje ETL spark datoteka. Uputa dodana na vrhu uputa.txt

---
+++ Apache Spark/run_scripts.py
@@ -0,0 +1,21 @@
+import subprocess
+
+# Define the container name
+container_name = "spark"
+
+# Define the commands as lists of arguments
+commands = [
+    ["docker", "exec", container_name, "spark-submit", "--master", "local[*]", "--driver-class-path", "/app/postgresql-42.7.1.jar", "/app/Apache Spark/spark_proizvodi_2.py"],
+    ["docker", "exec", container_name, "spark-submit", "--master", "local[*]", "--driver-class-path", "/app/postgresql-42.7.1.jar", "/app/Apache Spark/spark_trgovine_2.py"],
+    ["docker", "exec", container_name, "spark-submit", "--master", "local[*]", "--driver-class-path", "/app/postgresql-42.7.1.jar", "/app/Apache Spark/spark_vrijeme_2.py"],
+    ["docker", "exec", container_name, "spark-submit", "--master", "local[*]", "--driver-class-path", "/app/postgresql-42.7.1.jar", "/app/Apache Spark/spark_transakcije_2.py"]
+]
+
+# Run the commands sequentially
+for command in commands:
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command: {' '.join(command)}")
+        print(f"Error details: {e}")
+        break
